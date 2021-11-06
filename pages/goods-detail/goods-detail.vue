eb11d1756b405c9888f21aff78284eb7429795f4
Waiting-lwt <waiting_lwt@126.com>
2021-11-06 13:20:20 +0800

    update good-items

---
+++ pages/goods-detail/goods-detail.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				goodItem:{}
+			}
+		},
+		methods: {
+			
+		},
+		/**
+		 * 生命周期函数--监听页面加载
+		 */
+		
+		onLoad(option) {
+			// decodeURIComponent 解密传过来的对象字符串
+			if(JSON.stringify(option) != "{}"){
+				this.goodItem = JSON.parse(decodeURIComponent(option.goodItem));
+			}
+			console.log(this.goodItem)
+		},
+	}
+</script>
+
+<style>
+
+</style>
